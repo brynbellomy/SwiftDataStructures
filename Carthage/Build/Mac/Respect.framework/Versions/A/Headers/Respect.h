01491f9341557b0cdc17bd598e7fecdb0e17ee97
bryn austin bellomy <bryn@signals.io>
2015-01-17 10:46:41 -0600

    adding carthage frameworks

---
+++ Carthage/Build/Mac/Respect.framework/Versions/A/Headers/Respect.h
@@ -0,0 +1,19 @@
+//
+//  Respect.h
+//  Respect
+//
+//  Created by bryn austin bellomy on 2015 Jan 16.
+//  Copyright (c) 2015 bryn austin bellomy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Respect.
+FOUNDATION_EXPORT double RespectVersionNumber;
+
+//! Project version string for Respect.
+FOUNDATION_EXPORT const unsigned char RespectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Respect/PublicHeader.h>
+
+
